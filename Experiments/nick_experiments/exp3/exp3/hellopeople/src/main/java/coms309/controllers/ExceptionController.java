910bdb25e1e41ae0a24fbadc7eead1dd5b44d2c0
jacobnic6 <nicjacobs6@gmail.com>
2024-09-13 00:57:51 -0500

    Structuring classes, h2 db works

--- Experiments/nick_experiments/exp3/exp3/hellopeople/src/main/java/coms309/controllers/ExceptionController.java
+++ Experiments/nick_experiments/exp3/exp3/hellopeople/src/main/java/coms309/controllers/ExceptionController.java
@@ -1,5 +1,4 @@
 package coms309.controllers;
-
 /**
  * Controller used to showcase what happens when an exception is thrown
  *
@@ -11,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class ExceptionController {
+class ExceptionController
+    {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/oops")
-    public String triggerException() {
-        throw new RuntimeException("Check to see what happens when an exception is thrown");
+        @RequestMapping(method = RequestMethod.GET, path = "/oops")
+        public String triggerException()
+            {
+                throw new RuntimeException("Check to see what happens when an exception is thrown");
+            }
     }
-
-}
--- Experiments/nick_experiments/exp3/exp3/hellopeople/src/main/java/coms309/people/Person.java
+++ Experiments/nick_experiments/exp3/exp3/hellopeople/src/main/java/coms309/people/Person.java
@@ -10,77 +10,99 @@ import jakarta.persistence.Id;
  * @author Nicholas Jacobs
  */
 
-
 @Entity
-public class Person {
+public class Person
+    {
 
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer personId;
+        private String firstName;
 
+        private String lastName;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer personId;
-    private String firstName;
+        private String address;
 
-    private String lastName;
+        private String telephone;
 
-    private String address;
+        public Person()
+            {
+            }
 
-    private String telephone;
+        public Person(String firstName, String lastName, String address, String telephone)
+            {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.address = address;
+                this.telephone = telephone;
+            }
 
-    public Person(){}
+        public Integer getPersonId()
+            {
+                return personId;
+            }
 
-    public Person(String firstName, String lastName, String address, String telephone){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.telephone = telephone;
+        public void setPersonId(Integer personId)
+            {
+                this.personId = personId;
+            }
+
+        @Override
+        public boolean equals(Object o)
+            {
+                Person person = (Person) o;
+                return person.getFirstName().compareToIgnoreCase(firstName) == 0 && person.getLastName()
+                        .compareToIgnoreCase(lastName) == 0
+                        && person.getAddress().compareToIgnoreCase(address) == 0 && person.getTelephone()
+                        .compareToIgnoreCase(telephone) == 0;
+            }
+
+        public String getFirstName()
+            {
+                return this.firstName;
+            }
+
+        public void setFirstName(String firstName)
+            {
+                this.firstName = firstName;
+            }
+
+        public String getLastName()
+            {
+                return this.lastName;
+            }
+
+        public void setLastName(String lastName)
+            {
+                this.lastName = lastName;
+            }
+
+        public String getAddress()
+            {
+                return this.address;
+            }
+
+        public void setAddress(String address)
+            {
+                this.address = address;
+            }
+
+        public String getTelephone()
+            {
+                return this.telephone;
+            }
+
+        public void setTelephone(String telephone)
+            {
+                this.telephone = telephone;
+            }
+
+        @Override
+        public String toString()
+            {
+                return firstName + " "
+                        + lastName + " "
+                        + address + " "
+                        + telephone;
+            }
     }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getTelephone() {
-        return this.telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-    public Integer getPersonId()
-        {
-            return personId;
-        }
-
-    public void setPersonId(Integer personId)
-        {
-            this.personId = personId;
-        }
-    @Override
-    public String toString() {
-        return firstName + " " 
-               + lastName + " "
-               + address + " "
-               + telephone;
-    }
-}
