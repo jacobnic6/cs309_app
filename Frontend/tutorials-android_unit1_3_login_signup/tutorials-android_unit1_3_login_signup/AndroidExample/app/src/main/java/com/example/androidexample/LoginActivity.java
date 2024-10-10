21ec46c1c171dc7fa19635ef534555e1bf619205
Anthony Nehring <anehring@iastate.edu>
2024-10-10 17:56:55 +0000

    Revert "errors with build.gradle? Implementing the UserProfileActivty - and .xml"
    This reverts commit 16067077d8894f2317db5d6496fea1a2c462dbd9

--- Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -8,88 +8,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.androidexample.UserProfileActivity;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.HashMap;
-import java.util.Map;
-/*
 public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameEditText;  // define username edittext variable
     private EditText passwordEditText;  // define password edittext variable
     private Button loginButton;         // define login button variable
     private Button signupButton;        // define signup button variable
-    private boolean loginSuccessful = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);            // link to Login activity XML
 
-        // initialize UI elements
+        /* initialize UI elements */
         usernameEditText = findViewById(R.id.login_username_edt);
         passwordEditText = findViewById(R.id.login_password_edt);
         loginButton = findViewById(R.id.login_login_btn);    // link to login button in the Login activity XML
         signupButton = findViewById(R.id.login_signup_btn);  // link to signup button in the Login activity XML
 
-        // click listener on login button pressed
+        /* click listener on login button pressed */
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                // grab strings from user inputs
+                /* grab strings from user inputs */
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                String url = "http://coms-3090-058.class.las.iastate.edu:8080/users?username=" + username + "&password=" + password;
-
-                RequestQueue queue = Volley.newRequestQueue(this);
-
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                try {
-                                    JSONObject jsonObject = new JSONObject(response);
-                                    boolean loginSuccessful = jsonObject.getBoolean("success"); // Assuming your API returns a "success" flag
-
-                if (loginSuccessful) {
-                    // when login button is pressed, use intent to switch to Login Activity
-                    Intent intent = new Intent(LoginActivity.this, UserProfileActivity.class);
-                    intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
-                    intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
-                    startActivity(intent);  // go to MainActivity with the key-value data
-                }
-                else {
-                    // Handle login failure
-                    // You can show an error message to the user
-                }
-                catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                                    })
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
+                intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
+                startActivity(intent);  // go to MainActivity with the key-value data
             }
         });
 
-        // click listener on signup button pressed
+        /* click listener on signup button pressed */
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //when signup button is pressed, use intent to switch to Signup Activity
+                /* when signup button is pressed, use intent to switch to Signup Activity */
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);  // go to SignupActivity
             }
         });
     }
 }
-
-*/
--- Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/UserProfileActivity.java
+++ Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/UserProfileActivity.java
@@ -21,9 +21,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -33,69 +30,13 @@ import java.util.Map;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    private EditText weightInput;
-    private Button submitButton;
-    private GraphView weightGraph;
-    private double xValue = 0;
-    private static final int MAX_DATA_POINTS = 100;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_userprofile);
-
-        weightInput = findViewById(R.id.input_weight);
-        submitButton = findViewById(R.id.submit_input_weight);
-        weightGraph = findViewById(R.id.graph);
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String weight = weightInput.getText().toString();
-                submitWeightData(weight);
-            }
-        });
-    }
-
-    private void submitWeightData(String weight) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://coms-3090-058.class.las.iastate.edu:8080/users";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Handle successful response (e.g., update graph)
-                        androidx.media3.common.util.Log.d("Success", response);
-                        updateGraph(weight);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // Handle error
-                androidx.media3.common.util.Log.e("Error", error.toString());
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("weight", weight);
-                // Add other user data as needed
-                return params;
-            }
-        };
-        queue.add(stringRequest);
-    }
-
-    private void updateGraph(String weight) {
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
-        series.appendData(new DataPoint(xValue++, Double.parseDouble(weight)), true, MAX_DATA_POINTS);
-        weightGraph.addSeries(series); // Add series to the graph
-    }
-}
 
 
-
+    /* The Following buttons will be used to switch the intent to the respective page*/
+    private Button SocialButton;      // Define Social button variable
+    private Button ExerciseButton;    // Define Exercise button variable
+    private Button NutritionButton;   // Define Nutrition button variable
+    private Button SettingsButton;    // Define Settings button variable
 
 
     /* The following is for switching to the other four "main pages of the app" - social, exercise, nutrition, and settings
@@ -156,5 +97,5 @@ public class UserProfileActivity extends AppCompatActivity {
     }
 
      */
-
+}
 
