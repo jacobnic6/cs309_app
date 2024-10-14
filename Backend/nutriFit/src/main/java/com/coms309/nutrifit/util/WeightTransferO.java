45c74f86d0ca03f128c8b2a13580bdfd55f62a23
jacobnic6 <109706090+jacobnic6@users.noreply.github.com>
2024-10-14 15:44:20 -0500

    added user workoyt

---
+++ Backend/nutriFit/src/main/java/com/coms309/nutrifit/util/WeightTransferO.java
@@ -0,0 +1,15 @@
+package com.coms309.nutrifit.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WeightTransferO
+    {
+
+        double weight;
+    }
