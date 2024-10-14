3826e24b90ce84afd1c2d17770df412505914a3d
anehring <anehring@iastate.edu>
2024-10-13 20:13:38 -0500

    almost completed LoginActivity - erros

--- Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
+++ Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/LoginActivity.java
@@ -7,8 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.androidexample.main_five_pages.UserProfileActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +24,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;  // define password edittext variable
     private Button loginButton;         // define login button variable
     private Button signupButton;        // define signup button variable
+
+    private String loginUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +47,11 @@ public class LoginActivity extends AppCompatActivity {
                 String username = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                /* when login button is pressed, use intent to switch to Login Activity */
-                Intent intent = new Intent(LoginActivity.this, UserProfileActivity.class);
-                intent.putExtra("USERNAME", username);  // key-value to pass to the MainActivity
-                intent.putExtra("PASSWORD", password);  // key-value to pass to the MainActivity
-                startActivity(intent);  // go to MainActivity with the key-value data
+                // Need to implement here the volley logic and checkers for the requirements to login
+                checkLoginCredentials(username, password);
             }
         });
+
 
         /* click listener on signup button pressed */
         signupButton.setOnClickListener(new View.OnClickListener() {
@@ -56,4 +64,35 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-}
+
+        private void checkLoginCredentials(String username, String password){
+            String url = loginUrl + "?username=" + username + "&password=" + password;
+            StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonRespone = new JSONObject(response);
+                    boolean success = jsonRespone.getBoolean("success");
+
+                    if (success) {
+                        Intent intent = new Intent(LoginActivity.this, UserProfileActivity.class);
+                        intent.putExtra("Username", username);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(LoginActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(LoginActivity.this, "Error during login: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    });
+                    VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+            }
+            }
--- Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/main_five_pages/SocialActivity.java
+++ Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/main_five_pages/SocialActivity.java
@@ -1,4 +1,50 @@
 package com.example.androidexample.main_five_pages;
 
-public class SocialActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidexample.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class SocialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_social);
+
+    // The following is for switching to the other four "main pages of the app" - social, exercise, nutrition, and settings
+    BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+    bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected (@NonNull MenuItem item)
+        {
+            int itemId = item.getItemId();
+
+                if (itemId == R.id.social) {
+            startActivity(new Intent(SocialActivity.this, SocialActivity.class));
+            return true;
+        } else if (itemId == R.id.workouts) {
+            startActivity(new Intent(SocialActivity.this, WorkoutActivity.class));
+            return true;
+        } else if (itemId == R.id.profile) {
+            startActivity(new Intent(SocialActivity.this, UserProfileActivity.class));
+            return true;
+        } else if (itemId == R.id.nutrition) {
+            startActivity(new Intent(SocialActivity.this, NutritionActivity.class));
+            return true;
+        } else if (itemId == R.id.settings) {
+            startActivity(new Intent(SocialActivity.this, SettingsActivity.class));
+            return true;
+        }
+                return false;
+        }
+    });
+    }
 }
