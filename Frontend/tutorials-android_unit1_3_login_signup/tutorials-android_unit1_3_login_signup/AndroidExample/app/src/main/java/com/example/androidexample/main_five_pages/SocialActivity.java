65b1e0c4a200a2ca1248364be05a93d24cb627e3
anehring <anehring@iastate.edu>
2024-11-07 19:26:00 -0600

    search bar implemented

--- Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/main_five_pages/SocialActivity.java
+++ Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/main_five_pages/SocialActivity.java
@@ -1,96 +1,130 @@
 package com.example.androidexample.main_five_pages;
 
-import static com.example.androidexample.R.id.searchView;
-
 import android.annotation.SuppressLint;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.example.androidexample.R;
+import com.example.androidexample.VolleySingleton;
 import com.example.androidexample.main_five_pages.SettingsActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.search.SearchBar;
-import com.google.android.material.search.SearchView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.VolleyError;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SocialActivity extends AppCompatActivity {
-
-    private SearchView searchView;
-    private RecyclerView recyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
-        searchView = findViewById(R.id.searchView);
-        searchView.clearFocus();
-        searchView.setOnMenuItemClickListener(new SearchView.() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                if (!TextUtils.isEmpty(query)) {
-                    searchForFriends(query);
-                }
-            }
 
+        EditText searchBar = findViewById(R.id.search_bar);
+        Button searchButton = findViewById(R.id.search_button);
+
+        Button chatButton = findViewById(R.id.chat_button);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextChange(String newText) {
-                if (!TextUtils.isEmpty(newText)) {
-                    searchForFriends(newText);
+            public void onClick(View v) {
+                String searchQuery = searchBar.getText().toString();
+                if (!TextUtils.isEmpty(searchQuery)) {
+                    searchForFriends(searchQuery);
                 }
-                return true;
             }
         });
 
-    recyclerView = findViewById(R.id.recyclerView);
-    recyclerView.setHasFixedSize(true);
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // The following is for switching to the other four "main pages of the app" - social, exercise, nutrition, and settings
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
 
-
-    // The following is for switching to the other four "main pages of the app" - social, exercise, nutrition, and settings
-    BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-    bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected (@NonNull MenuItem item)
-        {
-            int itemId = item.getItemId();
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
 
                 if (itemId == R.id.social) {
-            startActivity(new Intent(SocialActivity.this, SocialActivity.class));
-            return true;
-        } else if (itemId == R.id.workouts) {
-            startActivity(new Intent(SocialActivity.this, WorkoutActivity.class));
-            return true;
-        } else if (itemId == R.id.profile) {
-            startActivity(new Intent(SocialActivity.this, UserProfileActivity.class));
-            return true;
-        } else if (itemId == R.id.nutrition) {
-            startActivity(new Intent(SocialActivity.this, NutritionActivity.class));
-            return true;
-        } else if (itemId == R.id.settings) {
-            startActivity(new Intent(SocialActivity.this, SettingsActivity.class));
-            return true;
-        }
+                    startActivity(new Intent(SocialActivity.this, SocialActivity.class));
+                    return true;
+                } else if (itemId == R.id.workouts) {
+                    startActivity(new Intent(SocialActivity.this, WorkoutActivity.class));
+                    return true;
+                } else if (itemId == R.id.profile) {
+                    startActivity(new Intent(SocialActivity.this, UserProfileActivity.class));
+                    return true;
+                } else if (itemId == R.id.nutrition) {
+                    startActivity(new Intent(SocialActivity.this, NutritionActivity.class));
+                    return true;
+                } else if (itemId == R.id.settings) {
+                    startActivity(new Intent(SocialActivity.this, SettingsActivity.class));
+                    return true;
+                }
                 return false;
+            }
+        });
+    }
+
+        private void searchForFriends (String searchQuery){
+            String url = "http://coms-3090-058.class.las.iastate.edu:8080/users/username/" + searchQuery;
+            JsonObjectRequest jsonObjReq = new JsonObjectRequest( Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d("Volley Response", response.toString());
+                            Toast.makeText(SocialActivity.this, "User found", Toast.LENGTH_SHORT).show();
+                            // Handle the response here
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("Volley Error", error.toString());
+                            Toast.makeText(SocialActivity.this, "User not found", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            ) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<String, String>();
+//                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+//                headers.put("Content-Type", "application/json");
+                    return headers;
+                }
+
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+//                params.put("param1", "value1");
+//                params.put("param2", "value2");
+                    return params;
+                }
+            };
+
+            // Adding request to request queue
+            VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
         }
-    });
-    }
 
-
-
-    private void searchForFriends(String query) {
-    String url = "" + query;
-
-
-    }
 }
