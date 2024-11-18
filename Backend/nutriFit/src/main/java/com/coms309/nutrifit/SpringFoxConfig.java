903f5b99d5b5d56971355ad996f8e6b8a7f152a0
jacobnic6 <nicjacobs6@gmail.com>
2024-11-18 00:28:40 -0600

    Created Muscle Progress tracker

--- Backend/nutriFit/src/main/java/com/coms309/nutrifit/SpringFoxConfig.java
+++ Backend/nutriFit/src/main/java/com/coms309/nutrifit/SpringFoxConfig.java
@@ -1,17 +1,22 @@
-package com.coms309.nutrifit.config;
+package com.coms309.nutrifit;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @Configuration
-public class SpringFoxConfig
-    {
+public class SpringFoxConfig{
+
         @Bean
         public Docket api() {
             return new Docket(DocumentationType.SWAGGER_2)
@@ -20,4 +25,7 @@ public class SpringFoxConfig
                     .paths(PathSelectors.any())
                     .build();
         }
+
+
+
     }
