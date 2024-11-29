86e48bfa4e01c8d91582e1e8f944718763993981
jacobnic6 <nicjacobs6@gmail.com>
2024-11-29 03:10:04 -0600

    improving parser

--- Backend/nutriFit/src/main/java/com/coms309/nutrifit/service/ExerciseServiceHandler.java
+++ Backend/nutriFit/src/main/java/com/coms309/nutrifit/service/ExerciseServiceHandler.java
@@ -12,8 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,19 +42,6 @@ public class ExerciseServiceHandler {
 		this.equipmentRepository = equipmentRepository;
 		this.objectMapper = objectMapper;
 		this.muscleRepository = muscleRepository;
-	}
-
-	/**
-	 * Import exercises.
-	 *
-	 * @param path the path
-	 *
-	 * @throws IOException the io exception
-	 */
-	public void importExercises(String path) throws IOException {
-		File file = new File(path);
-		List<Exercise> exerciseList = objectMapper.readValue(file, List.class);
-		exerciseRepository.saveAll(exerciseList);
 	}
 
 	/**
--- Backend/nutriFit/src/test/java/com/coms309/nutrifit/NutriFitApplicationTests.java
+++ Backend/nutriFit/src/test/java/com/coms309/nutrifit/NutriFitApplicationTests.java
@@ -14,6 +14,7 @@ class NutriFitApplicationTests {
 	 */
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
