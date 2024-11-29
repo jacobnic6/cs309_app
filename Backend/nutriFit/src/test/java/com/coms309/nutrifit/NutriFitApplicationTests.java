d8cbeca9835510bf388fe7b649f44a9dfc68354b
jacobnic6 <nicjacobs6@gmail.com>
2024-11-29 06:15:44 -0600

    changing config

--- Backend/nutriFit/src/test/java/com/coms309/nutrifit/NutriFitApplicationTests.java
+++ Backend/nutriFit/src/test/java/com/coms309/nutrifit/NutriFitApplicationTests.java
@@ -1,20 +1,27 @@
-//package com.coms309.nutrifit;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-/// **
-// * The type Nutri fit application tests.
-// */
-//@SpringBootTest
-//class NutriFitApplicationTests {
-//
-//	/**
-//	 * Context loads.
-//	 */
-//	@Test
-//	void contextLoads() {
-//
-//	}
-//
-//}
+package com.coms309.nutrifit;
+
+import com.coms309.nutrifit.controller.UserController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * The type Nutri fit application tests.
+ */
+@SpringBootTest
+class NutriFitApplicationTests {
+
+	@Autowired
+	private UserController userController;
+
+	/**
+	 * Context loads.
+	 */
+	@Test
+	void contextLoads() {
+		assertThat(userController).isNotNull();
+	}
+
+}
