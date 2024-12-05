9b4b8b4d3a7031afd44fcb2789a301d7852d8ca2
msbecker28 <msbecker@iastate.edu>
2024-12-05 03:57:43 -0600

    profile changes

--- Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/main_five_pages/UserProfileActivity.java
+++ Frontend/tutorials-android_unit1_3_login_signup/tutorials-android_unit1_3_login_signup/AndroidExample/app/src/main/java/com/example/androidexample/main_five_pages/UserProfileActivity.java
@@ -3,70 +3,84 @@ package com.example.androidexample.main_five_pages;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-
-
 import com.example.androidexample.R;
 import com.example.androidexample.VolleySingleton;
-import com.example.androidexample.main_five_pages.SettingsActivity;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.ArrayList;
 
 public class UserProfileActivity extends AppCompatActivity {
 
-    private EditText weightInput;
-    private Button postWeightButton;
-    private TextView pastWeightsTextView;
-    //private LineChart weightChart;
-    private String url = "http://coms-3090-058.class.las.iastate.edu:8080/bodyweights/username";
-    // private String url = "http://3a3a3fa2-d4e1-4281-8a26-1ee024d50f35.mock.pstmn.io";
+    private EditText weightInput, heightInput, bioInput, nameInput, ageInput, emailInput, goalInput;
+    private Button postWeightButton, saveProfileButton;
+    private TextView pastWeightsTextView, usernameText;
+    private ImageView profileImage;
     private String username;
-    //private float xValue = 0;
 
-
+    private String weightUrl = "http://coms-3090-058.class.las.iastate.edu:8080/bodyweights/username";
+    private String profileUrl = "http://coms-3090-058.class.las.iastate.edu:8080/profile/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userprofile);
 
+        // Get username from intent or set default for direct launch
         username = getIntent().getStringExtra("Username");
+        if (username == null) {
+            username = "default_username"; // Set your test username here
+        }
 
+        initializeViews();
+        setupClickListeners();
+        loadProfileData();
+        getWeightData();
+    }
+
+    private void initializeViews() {
+        // Profile views
+        nameInput = findViewById(R.id.name_input);
+        ageInput = findViewById(R.id.age_input);
+        heightInput = findViewById(R.id.height_input);
+        emailInput = findViewById(R.id.email_input);
+        goalInput = findViewById(R.id.goal_input);
+        bioInput = findViewById(R.id.bio_input);
+        saveProfileButton = findViewById(R.id.save_profile_button);
+
+        // Weight tracking views
         weightInput = findViewById(R.id.weight_input);
         postWeightButton = findViewById(R.id.post_weight_button);
-        //weightChart = findViewById(R.id.weight_chart);
         pastWeightsTextView = findViewById(R.id.past_weights_textview);
 
+        // User info views
+        usernameText = findViewById(R.id.username_text);
+        profileImage = findViewById(R.id.profile_image);
 
+        // Set username in UI
+        usernameText.setText(username);
+
+        setupBottomNavigation();
+    }
+
+    private void setupClickListeners() {
         postWeightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,39 +88,57 @@ public class UserProfileActivity extends AppCompatActivity {
             }
         });
 
-        getWeightData();
-
-        // The following is for switching to the other four "main pages of the app" - social, exercise, nutrition, and settings
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    int itemId = item.getItemId();
-
-                    if (itemId == R.id.social) {
-                        Intent intent = new Intent(UserProfileActivity.this, SocialActivity.class);
-                        intent.putExtra("Username", username);
-                        startActivity(intent);
-                        return true;
-                    } else if (itemId == R.id.workouts) {
-                        startActivity(new Intent(UserProfileActivity.this, WorkoutActivity.class));
-                        return true;
-                    } else if (itemId == R.id.profile) {
-                        startActivity(new Intent(UserProfileActivity.this, UserProfileActivity.class));
-                        return true;
-                    } else if (itemId == R.id.nutrition) {
-                        startActivity(new Intent(UserProfileActivity.this, NutritionActivity.class));
-                        return true;
-                    } else if (itemId == R.id.settings) {
-                        startActivity(new Intent(UserProfileActivity.this, SettingsActivity.class));
-                        return true;
-                    }
-                    return false;
-                }
+        saveProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateProfile();
+            }
         });
     }
 
+    private void loadProfileData() {
+        String url = profileUrl + username;
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (response.has("name")) {
+                                nameInput.setText(response.getString("name"));
+                            }
+                            if (response.has("age")) {
+                                ageInput.setText(response.getString("age"));
+                            }
+                            if (response.has("height")) {
+                                heightInput.setText(response.getString("height"));
+                            }
+                            if (response.has("email")) {
+                                emailInput.setText(response.getString("email"));
+                            }
+                            if (response.has("goal")) {
+                                goalInput.setText(response.getString("goal"));
+                            }
+                            if (response.has("bio")) {
+                                bioInput.setText(response.getString("bio"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(UserProfileActivity.this,
+                                    "Error parsing profile data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(UserProfileActivity.this,
+                                "Error loading profile: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
 
     private void postWeight() {
         String weight = weightInput.getText().toString();
@@ -123,11 +155,12 @@ public class UserProfileActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, postBody,
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, weightUrl, postBody,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(UserProfileActivity.this, "Weight posted successfully", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(UserProfileActivity.this,
+                                "Weight posted successfully", Toast.LENGTH_SHORT).show();
                         getWeightData();
                         weightInput.setText("");
                     }
@@ -135,47 +168,121 @@ public class UserProfileActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(UserProfileActivity.this, "Error posting weight: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(UserProfileActivity.this,
+                                "Error posting weight: " + error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
 
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
 
-        private void getWeightData() {
-            StringRequest request = new StringRequest(Request.Method.GET, url,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONArray jsonArray = new JSONArray(response);
-                                StringBuilder weightsBuilder = new StringBuilder();
+    private void getWeightData() {
+        StringRequest request = new StringRequest(Request.Method.GET, weightUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            StringBuilder weightsBuilder = new StringBuilder();
 
-                                for (int i = 0; i < jsonArray.length(); i++) {
-                                    JSONObject dataPoint = jsonArray.getJSONObject(i);
-                                    float weight = (float) dataPoint.getDouble("weight");
-                                    weightsBuilder.append(weight);
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject dataPoint = jsonArray.getJSONObject(i);
+                                float weight = (float) dataPoint.getDouble("weight");
+                                weightsBuilder.append(weight);
 
-                                    if (i < jsonArray.length() - 1) {
-                                        weightsBuilder.append(", ");
-                                    }
+                                if (i < jsonArray.length() - 1) {
+                                    weightsBuilder.append(", ");
                                 }
-
-                                pastWeightsTextView.setText(weightsBuilder.toString()); // Set text to TextView
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                Toast.makeText(com.example.androidexample.main_five_pages.UserProfileActivity.this, "Error parsing weight data", Toast.LENGTH_SHORT).show();
                             }
-                        }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(com.example.androidexample.main_five_pages.UserProfileActivity.this, "Error fetching weight data: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
 
-            VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
-        }
+                            pastWeightsTextView.setText(weightsBuilder.toString());
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(UserProfileActivity.this,
+                                    "Error parsing weight data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(UserProfileActivity.this,
+                                "Error fetching weight data: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
+
+    private void updateProfile() {
+        String url = profileUrl + username;
+
+        JSONObject profileData = new JSONObject();
+        try {
+            profileData.put("name", nameInput.getText().toString());
+            profileData.put("age", ageInput.getText().toString());
+            profileData.put("height", heightInput.getText().toString());
+            profileData.put("email", emailInput.getText().toString());
+            profileData.put("goal", goalInput.getText().toString());
+            profileData.put("bio", bioInput.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, profileData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(UserProfileActivity.this,
+                                "Profile updated successfully", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(UserProfileActivity.this,
+                                "Error updating profile: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+
+    private void setupBottomNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                Intent intent;
+
+                if (itemId == R.id.social) {
+                    intent = new Intent(UserProfileActivity.this, SocialActivity.class);
+                    intent.putExtra("Username", username);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.workouts) {
+                    intent = new Intent(UserProfileActivity.this, WorkoutActivity.class);
+                    intent.putExtra("Username", username);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.profile) {
+                    return true; // Already on profile
+                } else if (itemId == R.id.nutrition) {
+                    intent = new Intent(UserProfileActivity.this, NutritionActivity.class);
+                    intent.putExtra("Username", username);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.settings) {
+                    intent = new Intent(UserProfileActivity.this, SettingsActivity.class);
+                    intent.putExtra("Username", username);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
